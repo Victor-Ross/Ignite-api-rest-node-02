a0ee2f94911e2daee4c786ef788b5ebc41f45b80
Victor Ross <victor2ross@gmail.com>
2023-03-17 02:20:46 -0300

    First commit

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.string(),
+  DATABASE_URL: z.string(),
+  PORT: z.string().default('3333').transform(Number),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.log('Invalid environment variables', _env.error.format());
+
+  throw new Error('Invalid environment variables');
+}
+
+export const env = _env.data;
